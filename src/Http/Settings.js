33a694c00fd80f3df4297c91865e6f925c840999
mrjk1d <jeon57896@gmail.com>
2024-05-09 19:42:46 +0600

    third commit

--- src/Http/Settings.js
+++ src/Http/Settings.js
@@ -1,6 +1,20 @@
 import axios from 'axios'
 
+
 const instance = axios.create({
     baseURL: 'https://165.227.147.154:8081/'
 })
 export default instance
+
+
+// import axios from 'axios';
+
+// const instance = axios.create({
+//   baseURL: 'https://165.227.147.154:8081/',
+//   httpsAgent: {
+//     rejectUnauthorized: false
+//   }
+// });
+
+// export default instance;
+
